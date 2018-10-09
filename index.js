50e004e88b3ef6190ec6c6a0ed3aeb2600aac57a
Justin Legoff <legoff.justin@gmail.com>
2018-10-09 11:58:35 +0200

    add color modifier

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-var Spectrum = require("./Spectrum");
-var spectrum = new Spectrum([], [], "FF0000", "FF0000");
+/*var Spectrum = require("./lib/Spectrum");
+var spectrum = new Spectrum([], [], "FF0000", "F00000", "FF0F00");
 spectrum.properties();
-var newColor = spectrum.getColor(50);
-console.log(newColor);
+var newColor = spectrum.getColor(100);
+console.log(newColor);*/
+
+var ColorChanger = require("./lib/ColorChanger")
+
+console.log(ColorChanger.light("#123456", 150)) ;
